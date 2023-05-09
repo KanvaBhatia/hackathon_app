fe4c35160343e2d069d05d8ba134caabae21e2ee
Kanva Bhatia <kbthebest181@gmail.com>
2023-05-09 21:49:57 +0530

    fixing

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py makemigrations && python manage.py migrate && gunicorn hackathon_app.wsgi
+web: python manage.py makemigrations && python manage.py migrate && python manage.py collectstatic && gunicorn hackathon_app.wsgi
--- hackathon_app/settings.py
+++ hackathon_app/settings.py
@@ -31,7 +31,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
 ALLOWED_HOSTS = ['*']
-CORS_ALLOWED_ORIGINS = [r"https://hackathonapp-production.up.railway.app$"]
+# CORS_ALLOWED_ORIGINS = [r"https://hackathonapp-production.up.railway.app$"]
 # CORS_ALLOW_ALL_ORIGINS = True
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
@@ -53,19 +53,18 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'submissions',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "hackathon_app.urls"
@@ -92,10 +91,21 @@ WSGI_APPLICATION = "hackathon_app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': Path(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': Path(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ["PGDATABASE"],
+        'USER': os.environ["PGUSER"],
+        'PASSWORD': os.environ["PGPASSWORD"],
+        'HOST': os.environ["PGHOST"],
+        'PORT': os.environ["PGPORT"],
     }
 }
 
