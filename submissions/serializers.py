04f4ef6398599933379e4fdfff21b1914b3b7566
Kanva Bhatia <kanva.bhatia@gmail.com>
2023-05-10 15:52:16 +0530

    Add 2 more endpoints

--- submissions/serializers.py
+++ submissions/serializers.py
@@ -14,10 +14,19 @@ class SubmissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Submission
         fields = ['id', 'name', 'summary', 'submission_file', 'submission_image', 'submission_link', 'hackathon']
+    
+    # modifying init so that user can make submissions only in the enrolled hackathons
+    def __init__(self, *args, **kwargs):
+        user = kwargs['context']['request'].user
+        super().__init__(*args, **kwargs)
+        self.fields['hackathon'].queryset = Hackathon.objects.filter(
+            pk__in = HackathonRegistration.objects.filter(
+            user = user
+        ).values_list('hackathon', flat=True))
+
     def create(self, validated_data):
         hackathon = validated_data.get('hackathon')
         naming = {"file" : 'submission_file', "image" : "submission_image", "link" : "submission_link"}
-        print(validated_data)
         if(validated_data.get(naming[hackathon.submission_type]) in (None, '') ):
             raise serializers.ValidationError("Submission type doesn't match the hackathon requirements.")
 
