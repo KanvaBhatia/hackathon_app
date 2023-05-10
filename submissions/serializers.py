cbc07bcf478aa560cee09a588df324936401821f
KanvaBhatia <56185464+KanvaBhatia@users.noreply.github.com>
2023-05-10 16:02:28 +0530

    Users enrolled added (#1)
    * Add 2 more endpoints
    
    * remove unnecessary import
    
    * add migrations folder

--- submissions/serializers.py
+++ submissions/serializers.py
@@ -1,6 +1,5 @@
-from rest_framework import serializers, viewsets, permissions, routers
+from rest_framework import serializers
 from .models import Hackathon, Submission, HackathonRegistration
-from django.contrib.auth.models import User
 
 # serializer for hackathon
 class HackathonSerializer(serializers.ModelSerializer):
@@ -14,10 +13,19 @@ class SubmissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Submission
         fields = ['id', 'name', 'summary', 'submission_file', 'submission_image', 'submission_link', 'hackathon']
+    
+    # modifying init so that user can make submissions only in the enrolled hackathons
+    def __init__(self, *args, **kwargs):
+        user = kwargs['context']['request'].user
+        super().__init__(*args, **kwargs)
+        self.fields['hackathon'].queryset = Hackathon.objects.filter(
+            pk__in = HackathonRegistration.objects.filter(
+            user = user
+        ).values_list('hackathon', flat=True))
+
     def create(self, validated_data):
         hackathon = validated_data.get('hackathon')
         naming = {"file" : 'submission_file', "image" : "submission_image", "link" : "submission_link"}
-        print(validated_data)
         if(validated_data.get(naming[hackathon.submission_type]) in (None, '') ):
             raise serializers.ValidationError("Submission type doesn't match the hackathon requirements.")
 
